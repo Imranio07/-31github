425ed66daf4b135a0fbff0c974589e94dd1f0381
Imranio07 <107305213+Imranio07@users.noreply.github.com>
2023-11-08 22:42:16 +0600

    Add files via upload
    preparing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
-import PostsList from "./features/posts/PostsList";
-import AddPostForm from "./features/posts/AddPostForm";
-import SinglePostPage from "./features/posts/SinglePostPage";
-import EditPostForm from "./features/posts/EditPostForm";
-import Layout from "./components/Layout";
-import { Routes, Route } from 'react-router-dom';
+import './index.css';
+import { createContext, useState, useEffect } from 'react';
+import Header from './components/Header';
+import Employees from './pages/Employees';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Customers from './pages/Customers';
+import Dictionary from './pages/Dictionary';
+import Definition from './pages/Definition';
+import NotFound from './components/NotFound';
+import Customer from './pages/Customer';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import { baseUrl } from './shared';
+
+export const LoginContext = createContext();
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
+    useEffect(() => {
+        function refreshTokens() {
+            if (localStorage.refresh) {
+                const url = baseUrl + 'api/token/refresh/';
+                fetch(url, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        refresh: localStorage.refresh,
+                    }),
+                })
+                    .then((response) => {
+                        return response.json();
+                    })
+                    .then((data) => {
+                        localStorage.access = data.access;
+                        localStorage.refresh = data.refresh;
+                        setLoggedIn(true);
+                    });
+            }
+        }
 
-        <Route index element={<PostsList />} />
+        const minute = 1000 * 60;
+        refreshTokens();
+        setInterval(refreshTokens, minute * 3);
+    }, []);
 
-        <Route path="post">
-          <Route index element={<AddPostForm />} />
-          <Route path=":postId" element={<SinglePostPage />} />
-          <Route path="edit/:postId" element={<EditPostForm />} />
-        </Route>
+    const [loggedIn, setLoggedIn] = useState(
+        localStorage.access ? true : false
+    );
 
-      </Route>
-    </Routes>
-  );
+    function changeLoggedIn(value) {
+        setLoggedIn(value);
+        if (value === false) {
+            localStorage.clear();
+        }
+    }
+
+    return (
+        <LoginContext.Provider value={[loggedIn, changeLoggedIn]}>
+            <BrowserRouter>
+                <Header>
+                    <Routes>
+                        <Route path="/employees" element={<Employees />} />
+                        <Route path="/dictionary" element={<Dictionary />} />
+                        <Route
+                            path="/dictionary/:search"
+                            element={<Definition key={new Date().getTime()} />}
+                        />
+                        <Route path="/customers" element={<Customers />} />
+                        <Route path="/customers/:id" element={<Customer />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/register" element={<Register />} />
+                        <Route path="/404" element={<NotFound />} />
+                        <Route path="*" element={<NotFound />} />
+                    </Routes>
+                </Header>
+            </BrowserRouter>
+        </LoginContext.Provider>
+    );
 }
 
 export default App;
