17d427362b5bb822c12f7ab5d47b1be43abfe136
Imranio07 <107305213+Imranio07@users.noreply.github.com>
2023-11-13 22:51:49 +0600

    Add files via upload
    yes

--- src/App.js
+++ src/App.js
@@ -1,82 +1,27 @@
-import './index.css';
-import { createContext, useState, useEffect } from 'react';
-import Header from './components/Header';
-import Employees from './pages/Employees';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Customers from './pages/Customers';
-import Dictionary from './pages/Dictionary';
-import Definition from './pages/Definition';
-import NotFound from './components/NotFound';
-import Customer from './pages/Customer';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import { baseUrl } from './shared';
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import ProductList from "./productList";
+import {Provider} from "react-redux"
+import store from "./redux/store"
+import Basket from "./basket";
 
-export const LoginContext = createContext();
+const routes = {
+  posts: "/posts",
+};
 
 function App() {
-    useEffect(() => {
-        function refreshTokens() {
-            if (localStorage.refresh) {
-                const url = baseUrl + 'api/token/refresh/';
-                fetch(url, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({
-                        refresh: localStorage.refresh,
-                    }),
-                })
-                    .then((response) => {
-                        return response.json();
-                    })
-                    .then((data) => {
-                        localStorage.access = data.access;
-                        localStorage.refresh = data.refresh;
-                        setLoggedIn(true);
-                    });
-            }
-        }
-
-        const minute = 1000 * 60;
-        refreshTokens();
-        setInterval(refreshTokens, minute * 3);
-    }, []);
-
-    const [loggedIn, setLoggedIn] = useState(
-        localStorage.access ? true : false
-    );
-
-    function changeLoggedIn(value) {
-        setLoggedIn(value);
-        if (value === false) {
-            localStorage.clear();
-        }
-    }
-
-    return (
-        <LoginContext.Provider value={[loggedIn, changeLoggedIn]}>
-            <BrowserRouter>
-                <Header>
-                    <Routes>
-                        <Route path="/employees" element={<Employees />} />
-                        <Route path="/dictionary" element={<Dictionary />} />
-                        <Route
-                            path="/dictionary/:search"
-                            element={<Definition key={new Date().getTime()} />}
-                        />
-                        <Route path="/customers" element={<Customers />} />
-                        <Route path="/customers/:id" element={<Customer />} />
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/register" element={<Register />} />
-                        <Route path="/404" element={<NotFound />} />
-                        <Route path="*" element={<NotFound />} />
-                    </Routes>
-                </Header>
-            </BrowserRouter>
-        </LoginContext.Provider>
-    );
+  return (
+    <>
+        <Provider store={store}>
+      <Routes>
+          <Route path="/" element={<Layout />} >
+                  <Route path="list-products" element={<ProductList />} />
+              <Route path="basket" element={<Basket/>} />
+          </Route>
+      </Routes>
+    </Provider>
+    </>
+  );
 }
 
 export default App;
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,13 @@
-import { Outlet } from 'react-router-dom';
-import Header from './Header';
-
-const Layout = () => {
-    return (
-        <>
-            <Header />
-            <main className="App">
-                <Outlet />
-            </main>
-        </>
-    )
+import { Outlet} from "react-router-dom";
+import Navbar from "./Navbar";
+const Layout= () => {
+  return (
+      <div>
+          <Navbar/>
+          <Outlet/>
+          <footer>Geeks 2023</footer>
+      </div>
+  );
 }
 
-export default Layout
+export default Layout;
