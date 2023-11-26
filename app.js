862e99a30c2d06b9b9a7d4b1d2d9fe81b85d6b29
Imranio07 <107305213+Imranio07@users.noreply.github.com>
2023-11-26 19:51:09 +0600

    Add files via upload
    js

--- app.js
+++ app.js
@@ -1,59 +1,24 @@
-// console.log('Hello')
+setTimeout(()=>{
+    console.log('hello5')
+},0)
 
+const helloSome=()=>{
+    console.log("hello")
+}
+console.log("hello2")
 
-//DOM -document object model
+setTimeout(()=> {
+    console.log('hello4')
+},1000)
 
-// console.log(document.body);
+const helloSomeTwo=()=>{
+    console.log('hello3')
+}
+helloSomeTwo()
+helloSome()
 
-// document.body.style.background = 'red'
-
-// const buttons = document.getElementsByClassName('btn')
-
-// const button = document.getElementById('btn1')
-// const button = document.getElementsByTagName('button')
-
-// buttons[0].style.background = 'red'
-//add() remove() contains()
-
-// console.log(buttons[0].classList)
-// buttons[0].classList.add('red')
-// buttons[0].classList.remove('red')
-
-// console.log()
-// const logger = () => console.log('click')
-//
-// buttons[0].addEventListener('click', () => {
-//     console.log('1')
-// })
-
-//
-// buttons.forEach(button => {
-//     button.onclick = (event) => {
-//         if(event.target.classList.contains('red')) {
-//             event.target.classList.remove('red')
-//         }else{
-//             event.target.classList.add('red')
-//         }
-//     }
-// })
-// const newButton = document.createElement('Button')
-// wrapper.append(newButton)
-
-// Делигирование событий
-
-
-const buttons = document.querySelectorAll('button')
-const wrapper = document.querySelectorAll('.btn-block')
-
-wrapper.addEventListener('click', (event) => {
-    const element =event.target()
-    if(event.target.tagName.toLowerCase()=== 'button'){
-        event.target.addEventListener('click', () => {
-            if(e.target.classList.contains('red')){
-                e.target.classList.remove('red')
-            }else{
-                e.target.classList.add('red')
-            }
-        })
-    }
-})
+//hello2
+//hello3
+//hello
+//hello5
+//hello4
